1d5d9285f43fa767f9a501e9c2cac895bc968e03
Gstars01 <khw4986@gmail.com>
2022-08-01 15:24:44 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	CMakeLists.txt

--- insert.c
+++ insert.c
@@ -1,3 +1,11 @@
 //
 // Created by khw49 on 2022-08-01.
 //
+#include <stdio.h>
+
+insert_element(L,n,x){
+    for(i)
+}
+int main(){
+
+}
---
+++ listS.h
@@ -0,0 +1,8 @@
+//
+// Created by khw49 on 2022-08-01.
+//
+
+#ifndef UNTITLED1_LISTS_H
+#define UNTITLED1_LISTS_H
+
+#endif //UNTITLED1_LISTS_H
